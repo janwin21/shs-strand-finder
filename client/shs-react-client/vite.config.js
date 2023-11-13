85de9930586a5dd0f4bb83aed71d838b73493cf4
unknown <janwinjava21@gmail.com>
2023-11-13 21:06:39 +0800

    shs-strand-finder fullstack v1.1.14

--- client/shs-react-client/vite.config.js
+++ client/shs-react-client/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+/*
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+*/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // or any value that suits your project
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
